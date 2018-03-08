69be97a1ed88f22bcd4934962e2863ab959bdce4
nydi <stefan.nydegger@gmail.com>
2018-03-08 23:48:06 +0100

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBsa8yanPD7B5QaomKiyWKL4a3nBgZXNw4',
+    authDomain: 'angular5firebasecrud-b9bd2.firebaseapp.com',
+    databaseURL: 'https://angular5firebasecrud-b9bd2.firebaseio.com',
+    projectId: 'angular5firebasecrud-b9bd2',
+    storageBucket: '',
+    messagingSenderId: '127304319610'
+  }
 };
