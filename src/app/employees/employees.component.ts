266822365b869ab37562dd4ed8e214399d531dd6
nydi <stefan.nydegger@gmail.com>
2018-03-10 15:27:34 +0100

    validation for age and salary

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EmployeeService} from './shared/employee.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-employees',
